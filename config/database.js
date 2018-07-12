3bc78bde9c55f55131dbd3d426ca2b3d499aefcd
henrypadua <henrybpj@live.com>
2018-07-12 17:41:58 -0300

    Env

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
-// module.exports = {
-//   username: 'grupo602_admin',
-//   password: 'dev159*',
-//   database: 'grupo602_portalunicel',
-//   host: 'kairospetro.com.br',
-//   dialect: 'mysql',
-// };
 module.exports = {
-  username: 'root',
-  password: 'root',
-  database: 'portalund',
-  host: '127.0.0.1',
+  username: 'grupo602_admin',
+  password: 'dev159*',
+  database: 'grupo602_portalunicel',
+  host: 'kairospetro.com.br',
   dialect: 'mysql',
-  operatorsAliases: false,
 };
+// module.exports = {
+//   username: 'root',
+//   password: 'root',
+//   database: 'portalund',
+//   host: '127.0.0.1',
+//   dialect: 'mysql',
+//   operatorsAliases: false,
+// };
